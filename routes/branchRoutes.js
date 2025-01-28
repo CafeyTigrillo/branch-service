d787e311756bc32584c70a6f2b9c80ae6ea8e9d0
Edgar <epgarzonr@uce.edu.ec>
2025-01-27 23:46:26 -0500

    chore: organize branchRoutes file

--- routes/branchRoutes.js
+++ routes/branchRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const { getBranchById } = require('../controllers/branchController');
 const router = express.Router();
-
-// Get branch details by ID (read-only)
 router.get('/:id_branch', getBranchById);
 
 module.exports = router;
